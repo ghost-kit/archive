373863f5592080b749ba2e7f682f9450b46d53ef
Joshua Murphy <joshua.murphy@lasp.colorado.edu>
2013-01-23 21:56:09 +0000

    End of day commit. Have update the UI with temp icons. Have starting linking new info panel to data sources. will be fully linked soon.

--- applications/SwFT/swftStatusWindowWidget.cpp
+++ applications/SwFT/swftStatusWindowWidget.cpp
@@ -1,14 +1,130 @@
 #include "swftStatusWindowWidget.h"
 #include "ui_swftStatusWindowWidget.h"
 
+#include <QHeaderView>
+#include <QLineEdit>
+#include <QStringList>
+
+#include "vtkCommand.h"
+#include "vtkEventQtSlotConnect.h"
+#include <vtksys/SystemTools.hxx>
+
+#include "vtkPVArrayInformation.h"
+#include "vtkPVCompositeDataInformation.h"
+#include "vtkPVDataInformation.h"
+#include "vtkPVDataSetAttributesInformation.h"
+#include "vtkSmartPointer.h"
+#include "vtkSMDomain.h"
+#include "vtkSMDomainIterator.h"
+#include "vtkSMDoubleVectorProperty.h"
+#include "vtkSMOutputPort.h"
+#include "vtkSMPropertyIterator.h"
+
+#include "pqActiveObjects.h"
+#include "pqNonEditableStyledItemDelegate.h"
+#include "pqOutputPort.h"
+#include "pqPipelineSource.h"
+#include "pqSMAdaptor.h"
+#include "pqServer.h"
+#include "pqTimeKeeper.h"
+
+
+#include <QVariant>
+
 swftStatusWindowWidget::swftStatusWindowWidget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::swftStatusWindowWidget)
 {
     ui->setupUi(this);
+
+    ui->modelNameLabel->setText("[___] Model Information");
+
+    this->VTKConnect = vtkEventQtSlotConnect::New();
+    this->updateInformation();
+
+    this->connect(&pqActiveObjects::instance(),
+                  SIGNAL (portChanged(pqOutputPort*)),
+                  this,
+                  SLOT(setOutputPort(pqOutputPort*)));
+
 }
 
 swftStatusWindowWidget::~swftStatusWindowWidget()
 {
+    this->VTKConnect->Disconnect();
+    this->VTKConnect->Delete();
     delete ui;
+}
+
+pqOutputPort *swftStatusWindowWidget::getOutputPort()
+{
+}
+
+
+void swftStatusWindowWidget::setOutputPort(pqOutputPort *source)
+{
+    if(this->OutputPort == source)
+    {
+        return;
+    }
+
+    this->VTKConnect->Disconnect();
+    if (this->OutputPort)
+    {
+        QObject::disconnect((this->OutputPort->getSource(),
+                             SIGNAL (dataUdated(pqPipelineSource*)),
+                             this, SLOT(updateInformation())));
+    }
+
+    this->OutputPort = source;
+    if(this->OutputPort)
+    {
+        QObject::connect(this->OutputPort->getSource(),
+                         SIGNAL(dataUpdated(pqPipelineSource*)),
+                         this, SLOT(updateInformation()));
+    }
+
+    this->updateInformation();
+
+}
+
+void swftStatusWindowWidget::fillDataInformation(vtkPVDataInformation *info)
+{
+
+    std::cout << __FUNCTION__ << " " << __LINE__ << " has been triggered" << std::endl;
+}
+
+
+void swftStatusWindowWidget::updateInformation()
+{
+
+    std::cout << "update Information triggered" << std::endl;
+
+    vtkPVDataInformation *dataInformation = NULL;
+    pqPipelineSource * source = NULL;
+
+    if(this->OutputPort)
+    {
+        source = this->OutputPort->getSource();
+        if(this->OutputPort->getOutputPortProxy())
+        {
+            dataInformation = this->OutputPort->getDataInformation();
+        }
+    }
+
+    if(!source || !dataInformation)
+    {
+        this->fillDataInformation(0);
+        return;
+    }
+
+    vtkPVCompositeDataInformation *compositeInformation = dataInformation->GetCompositeDataInformation();
+
+    if(compositeInformation->GetDataIsComposite())
+    {
+        //todo
+    }
+
+    //
+
 }
--- applications/SwFT/swftStatusWindowWidget.h
+++ applications/SwFT/swftStatusWindowWidget.h
@@ -2,6 +2,13 @@
 #define SWFTSTATUSWINDOWWIDGET_H
 
 #include <QWidget>
+#include <QPointer>
+#include "pqComponentsModule.h"
+
+class pqOutputPort;
+class QTreeWidgetItem;
+class vtkEventQtSlotConnect;
+class vtkPVDataInformation;
 
 namespace Ui {
 class swftStatusWindowWidget;
@@ -15,8 +22,25 @@ public:
     explicit swftStatusWindowWidget(QWidget *parent = 0);
     ~swftStatusWindowWidget();
     
+    pqOutputPort *getOutputPort();
+
+public slots:
+    void updateInformation();
+
+    void setOutputPort(pqOutputPort *source);
+
+private slots:
+
+
+private:
+    void fillDataInformation(vtkPVDataInformation* info);
+
 private:
     Ui::swftStatusWindowWidget *ui;
+
+    QPointer<pqOutputPort> OutputPort;
+    vtkEventQtSlotConnect *VTKConnect;
+
 };
 
 #endif // SWFTSTATUSWINDOWWIDGET_H
