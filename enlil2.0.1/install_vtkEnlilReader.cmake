95ad11e7ea50008023115e7257b19ff09a868f21
Joshua Murphy <joshua.murphy@lasp.colorado.edu>
2013-03-06 22:58:46 +0000

    some commented out modifications that got things to work on my workstation

--- enlil2.0.1/install_vtkEnlilReader.cmake
+++ enlil2.0.1/install_vtkEnlilReader.cmake
@@ -20,9 +20,10 @@ if (APPLE)
   execute_process(
       COMMAND ${PV_SUPERBUILD_LIST_DIR}/apple/fixup_plugin.py
             # The directory containing the plugin dylibs or the plugin itself.
-            ${TMP_DIR}/vtkEnlilReader//${SHARED_LIBRARY_PREFIX}vtkEnlilReader${SHARED_LIBRARY_SUFFIX}
+            ${TMP_DIR}/vtkEnlilReader/${SHARED_LIBRARY_PREFIX}vtkEnlilReader${SHARED_LIBRARY_SUFFIX}
             # names to replace (in order)
             "${PARAVIEW_BINARY_DIR}/lib/=@executable_path/../Libraries/"
+            #"/Users/jomu9721/Data/Development/SwFT/superbuild/paraview/src/paraview-build/lib/=@executable_path/../Libraries/"
             "${QT_LIBRARY_DIR}/Qt=@executable_path/../Frameworks/Qt"
             "${QT_LIBRARY_DIR}/=@executable_path/../Libraries/"
             "libhdf5.7.3.0.dylib=@executable_path/../Libraries/libhdf5.1.8.9.dylib"
