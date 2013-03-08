c87c5ef7281291c99c54f0d107d54fa96a6748b8
Joshua Murphy <joshua.murphy@lasp.colorado.edu>
2013-03-08 07:31:56 +0000

    moving hacks to home

--- enlil2.0.1/install_vtkEnlilReader.cmake
+++ enlil2.0.1/install_vtkEnlilReader.cmake
@@ -16,6 +16,8 @@ execute_process(COMMAND ${CMAKE_COMMAND} -E remove_directory ${TMP_DIR}/vtkEnlil
 execute_process(COMMAND ${CMAKE_COMMAND} -E make_directory ${TMP_DIR}/vtkEnlilReader)
 execute_process(COMMAND ${CMAKE_COMMAND} -E copy ${BINARY_DIR}/${SHARED_LIBRARY_PREFIX}vtkEnlilReader${SHARED_LIBRARY_SUFFIX} ${TMP_DIR}/vtkEnlilReader)
 
+
+#This is hacked for MacD108... not sure why it all works on other machines, but not this one...
 if (APPLE)
   execute_process(
       COMMAND ${PV_SUPERBUILD_LIST_DIR}/apple/fixup_plugin.py
@@ -23,7 +25,11 @@ if (APPLE)
             ${TMP_DIR}/vtkEnlilReader/${SHARED_LIBRARY_PREFIX}vtkEnlilReader${SHARED_LIBRARY_SUFFIX}
             # names to replace (in order)
             "${PARAVIEW_BINARY_DIR}/lib/=@executable_path/../Libraries/"
-            #"/Users/jomu9721/Data/Development/SwFT/superbuild/paraview/src/paraview-build/lib/=@executable_path/../Libraries/"
+            "/Users/jomu9721/Data/Development/SwFT/superbuild/install/lib/libavformat.dylib=@executable_path/../Libraries/libavformat.52.64.2.dylib"
+            "/Users/jomu9721/Data/Development/SwFT/superbuild/install/lib/libavcodec.dylib=@executable_path/../Libraries/libavcodec.52.72.2.dylib"
+            "/Users/jomu9721/Data/Development/SwFT/superbuild/install/lib/libavutil.dylib=@executable_path/../Libraries/libavutil.50.15.1.dylib"
+            "/Users/jomu9721/Data/Development/SwFT/superbuild/install/lib/libswscale.dylib=@executable_path/../Libraries/libswscale.0.11.0.dylib"
+            "/Users/jomu9721/Data/Development/SwFT/superbuild/paraview/src/paraview-build/lib/=@executable_path/../Libraries/"
             "${QT_LIBRARY_DIR}/Qt=@executable_path/../Frameworks/Qt"
             "${QT_LIBRARY_DIR}/=@executable_path/../Libraries/"
             "libhdf5.7.3.0.dylib=@executable_path/../Libraries/libhdf5.1.8.9.dylib"
